b29eb7fbf52ef7e15e59be99ebd304d6a97fc1d5
steev <mauricefl@outlook.de>
2023-02-11 22:22:23 +0100

    initial commit

---
+++ nervcore_firmware/src/config.h
@@ -0,0 +1,21 @@
+// weapon trigger
+#define trigger 16
+
+/** =================================
+ * launch drive
+================================== */
+// pwm pins for launch drive
+#define launchPWM_top 11
+#define launchPWM_bottom 12
+
+/** =================================
+ * Display
+================================== */
+// i2c pins for display
+#define dispSDA 21
+#define dispSCL 22
+
+// Selection interface for display
+#define selectorSelectA 17
+#define selectroSelectr 18
+#define selectorConfirm 8
